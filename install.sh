7f95b439e0d3e80858b00219bb26f3af29a7e56d
rattfieldnz <emailme@robert.attfield.com>
2025-07-28 12:04:08 +1200

    Initial scaffolding

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# install.sh
+
+composer require laravel/jetstream laravel/sanctum nuwave/lighthouse \
+                 maatwebsite/excel barryvdh/laravel-dompdf \
+                 filament/filament beyondcode/laravel-websockets
+
+# Jetstream + Livewire
+php artisan jetstream:install livewire --teams=false
+npm install && npm run dev
+
+# Publish vendor assets
+php artisan vendor:publish --tag=fortify-config
+php artisan vendor:publish --tag=filament-config
+php artisan vendor:publish --provider="BeyondCode\LaravelWebSockets\WebSocketsServiceProvider" --tag="config"
+
+php artisan migrate
